f9a5feb1910940b5ef36aeec863f1b5dd487fbf0
neennera <neennera@gmail.com>
2024-11-12 00:13:19 +0700

    feat: prep a fetch api

--- src/components/about/member-section.tsx
+++ src/components/about/member-section.tsx
@@ -1,8 +1,39 @@
+"use client";
 import { MemberButton } from "@/components/common/member-button";
 import { Section } from "@/components/common/section";
 import { Title } from "@/components/common/title";
+import { useEffect, useState } from "react";
 
 export const MemberSection = () => {
+  const [members, setMembers] = useState<GithubMemberProps[]>([]);
+  // const members: GithubMemberProps[] = [];
+  useEffect(() => {
+    const fetchAPI = async () => {
+      try {
+        // const { data }: { data: GithubMemberProps[] } = await axios.get(
+        //   `https://api.github.com/orgs/esc-chula/public_members`,
+        // );
+        const response = await fetch(
+          `https://api.github.com/orgs/esc-chula/public_members`,
+        );
+
+        console.log(response);
+        // const data = await response.json();
+        // console.log(data);
+        setMembers([]);
+        // setMembers((members) => {
+        //   return [...members, ...data].sort((a, b) =>
+        //     a.login.localeCompare(b.login),
+        //   );
+        // });
+        // console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAPI().catch(console.error);
+  }, []);
+
   return (
     <Section className="gap-6">
       <Title variant="sectionTitle" color="primary" className="text-4xl">
@@ -14,24 +45,35 @@ export const MemberSection = () => {
         veniam, quis nostrud exercitation
       </p>
       <div className="flex flex-wrap items-center justify-center gap-2">
-        {memberTemp.map((member, index) => (
-          <MemberButton key={index} nameTag={member} />
+        {members.map((member, index) => (
+          <MemberButton
+            key={index}
+            nameTag={member.login}
+            url={member.html_url}
+          />
         ))}
       </div>
     </Section>
   );
 };
 
-const memberTemp = [
-  "Jowkem #2 CEDT",
-  "GG #2 CP",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-  "Jowkem #2 CEDT",
-];
+interface GithubMemberProps {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
--- src/components/common/member-button.tsx
+++ src/components/common/member-button.tsx
@@ -1,8 +1,19 @@
-export const MemberButton = ({ nameTag }: { nameTag: string }) => {
+import Link from "next/link";
+
+export const MemberButton = ({
+  nameTag,
+  url,
+}: {
+  nameTag: string;
+  url: string;
+}) => {
   return (
-    <div className="flex h-14 items-center justify-start gap-4 rounded-lg bg-gradient-to-b from-neutral-700 to-neutral-800 px-4">
+    <Link
+      href={url}
+      className="flex h-14 items-center justify-start gap-4 rounded-lg bg-gradient-to-b from-neutral-700 to-neutral-800 px-4"
+    >
       <div className="h-10 w-10 rounded-full bg-white"></div>
       <p>{nameTag}</p>
-    </div>
+    </Link>
   );
 };
