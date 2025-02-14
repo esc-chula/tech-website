d1d2fe6fa56d4efb7ae2e52d3c050230ba6de46b
ebonian <52095091+ebonian@users.noreply.github.com>
2025-02-14 13:06:50 +0700

    fix(hackathon): admin page styling

--- src/app/(events)/hackathon/admin/ticket-filters.tsx
+++ src/app/(events)/hackathon/admin/ticket-filters.tsx
@@ -1,5 +1,7 @@
 import type { Dispatch, SetStateAction } from 'react';
 
+import { Button } from '~/components/ui/button';
+
 interface TicketFiltersProps {
   setFilter: Dispatch<SetStateAction<string>>;
 }
@@ -7,34 +9,10 @@ interface TicketFiltersProps {
 const TicketFilters = ({ setFilter }: TicketFiltersProps): JSX.Element => {
   return (
     <div className="flex space-x-4 items-center justify-center">
-      <button
-        className="w-32  rounded-md p-2 bg-slate-400 text-black hover:bg-slate-500 cursor-pointer"
-        type="button"
-        onClick={() => setFilter('ALL')}
-      >
-        All
-      </button>
-      <button
-        className="w-32  rounded-md p-2 bg-slate-400 text-black hover:bg-slate-500 cursor-pointer"
-        type="button"
-        onClick={() => setFilter('CLAIMED')}
-      >
-        Claimed
-      </button>
-      <button
-        className="w-32  rounded-md p-2 bg-slate-400 text-black hover:bg-slate-500 cursor-pointer"
-        type="button"
-        onClick={() => setFilter('UNCLAIMED')}
-      >
-        Unclaimed
-      </button>
-      <button
-        className="w-32  rounded-md p-2 bg-slate-400 text-black hover:bg-slate-500 cursor-pointer"
-        type="button"
-        onClick={() => setFilter('REGISTERED')}
-      >
-        Registered
-      </button>
+      <Button onClick={() => setFilter('ALL')}>All</Button>
+      <Button onClick={() => setFilter('CLAIMED')}>Claimed</Button>
+      <Button onClick={() => setFilter('UNCLAIMED')}>Unclaimed</Button>
+      <Button onClick={() => setFilter('REGISTERED')}>Registered</Button>
     </div>
   );
 };
