254ef4b59c3d52b3cb55f8c315eebeac3c36a306
ebonian <52095091+ebonian@users.noreply.github.com>
2025-02-14 13:06:50 +0700

    fix(hackathon): admin page styling

--- src/app/(events)/hackathon/admin/ticket-actions.tsx
+++ src/app/(events)/hackathon/admin/ticket-actions.tsx
@@ -1,4 +1,17 @@
+'use client';
+
 import type { HackathonTicketType } from '@prisma/client';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '~/components/ui/alert-dialog';
 
 interface Ticket {
   id: number;
@@ -23,13 +36,33 @@ const TicketActions = ({
   }
 
   return (
-    <button
-      className="bg-red-400 text-black font-semibold hover:bg-red-600 rounded-md p-2"
-      type="button"
-      onClick={deleteTicket}
-    >
-      Delete
-    </button>
+    <>
+      <button
+        className="bg-yellow-400 text-black font-semibold hover:bg-yellow-600 rounded-md p-2"
+        type="button"
+      >
+        Edit
+      </button>
+      <AlertDialog>
+        <AlertDialogTrigger className="bg-red-400 text-black font-semibold hover:bg-red-600 rounded-md p-2">
+          Delete
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-red-400 hover:bg-red-600"
+              onClick={deleteTicket}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
