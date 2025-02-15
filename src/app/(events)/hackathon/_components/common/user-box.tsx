3fcbd3622159c8cf4fa5eac0860cf6703a39f415
Tanadon Santisan <52095091+ebonian@users.noreply.github.com>
2025-02-15 12:24:44 +0700

    Hackathon backgrounds (#51)
    * feat: add pathname to req headers
    
    * feat(hackathon): registration backgrounds variant
    
    * fix(hackathon): parent user box client side

--- src/app/(events)/hackathon/_components/common/user-box.tsx
+++ src/app/(events)/hackathon/_components/common/user-box.tsx
@@ -1,86 +1,46 @@
-'use client';
-
-import { LoaderCircle, User } from 'lucide-react';
+import { User } from 'lucide-react';
+import { headers } from 'next/headers';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
-import { useToast } from '~/hooks/use-toast';
-import { getSession, logout } from '~/server/actions/auth';
+import { getSession } from '~/server/actions/auth';
 
-const UserBox: React.FC = () => {
-  const pathname = usePathname();
-  const router = useRouter();
+import LogoutButton from './logout-button';
 
-  const { toast } = useToast();
+const UserBox: React.FC = async () => {
+  const headersList = headers();
+  const pathname = headersList.get('x-pathname') ?? '';
 
-  const [loading, setLoading] = useState(true);
-  const [studentId, setStudentId] = useState<string | null>(null);
+  const res = await getSession();
+  if (!res.success) {
+    return (
+      <div className="flex items-center gap-2 h-8 z-40">
+        <div className="w-44 h-full flex items-center gap-2 px-4 rounded-full bg-white/20 border border-white/40 backdrop-blur-sm">
+          <User className="opacity-50" size={20} />
+          <span className="text-sm font-medium">Hi, Hackers</span>
+        </div>
 
-  useEffect(() => {
-    getSession()
-      .then((res) => {
-        setStudentId(res.success ? res.data.studentId : null);
-      })
-      .catch(() => {
-        setStudentId(null);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div className="flex items-center gap-2 h-8 z-40">
-      <div className="w-44 h-full flex items-center gap-2 px-4 rounded-full bg-white/20 border border-white/40 backdrop-blur-sm">
-        {loading ? (
-          <LoaderCircle className="animate-spin opacity-50" size={20} />
-        ) : (
-          <>
-            <User className="opacity-50" size={20} />
-            <span className="text-sm font-medium">
-              Hi, {studentId ?? 'Hackers'}
-            </span>
-          </>
-        )}
-      </div>
-      {studentId ? (
-        <button
-          className="rounded-full bg-hackathon-primary h-full px-5"
-          type="button"
-          onClick={() => {
-            logout()
-              .then(() => {
-                router.replace('/hackathon');
-              })
-              .catch(() => {
-                toast({
-                  title: 'Failed to logout',
-                  description: 'Please try again later.',
-                  variant: 'destructive',
-                });
-              });
-          }}
-        >
-          Logout
-        </button>
-      ) : (
         <Link
           className="rounded-full overflow-hidden h-full"
-          href={loading ? '#' : `/hackathon/login?redirectUrl=${pathname}`}
+          href={`/hackathon/login?redirectUrl=${pathname}`}
         >
           <button
             className="rounded-full bg-hackathon-primary h-full px-5"
             type="button"
           >
-            {loading ? (
-              <LoaderCircle className="animate-spin opacity-50" size={20} />
-            ) : (
-              'Login'
-            )}
+            Login
           </button>
         </Link>
-      )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center gap-2 h-8 z-40">
+      <div className="w-44 h-full flex items-center gap-2 px-4 rounded-full bg-white/20 border border-white/40 backdrop-blur-sm">
+        <User className="opacity-50" size={20} />
+        <span className="text-sm font-medium">Hi, {res.data.studentId}</span>
+      </div>
+      <LogoutButton />
     </div>
   );
 };
