893cdb18990e90dc2b6ee9f7b322aca1045f0fb8
shalluv <leon555thai@gmail.com>
2025-02-16 20:27:05 +0700

    feat: claiming logic

--- src/app/(events)/hackathon/_components/ticket/ticket-form.tsx
+++ src/app/(events)/hackathon/_components/ticket/ticket-form.tsx
@@ -1,16 +1,23 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { type SubmitHandler, useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { useToast } from '~/hooks/use-toast'
-import { claimHackahonTicketWithRateLimit } from '~/server/actions/hackathon'
-import { type HackathonTicket } from '~/types/hackathon'
+import {
+  claimHackahonTicketWithRateLimit,
+  createHackathonTeamTicket,
+} from '~/server/actions/hackathon'
+import {
+  type HackathonTicket,
+  type HackathonTicketClaim,
+} from '~/types/hackathon'
 import { type Response } from '~/types/server'
 
 import ClaimedTicket from './claimed-ticket'
+import CreateTeamBox from './create-team-box'
 import TicketBox from './ticket-box'
 
 export const codeSchema = z.object({
@@ -21,10 +28,22 @@ export const codeSchema = z.object({
 
 export type CodeSchema = z.infer<typeof codeSchema>
 
-const TicketForm: React.FC = () => {
+interface TicketFormProps {
+  ticket1?: {
+    id: HackathonTicket['id']
+    ticketType: HackathonTicket['ticketType']
+    expiredAt: HackathonTicketClaim['expiredAt']
+  }
+  ticket2?: {
+    id: HackathonTicket['id']
+    ticketType: HackathonTicket['ticketType']
+    expiredAt: HackathonTicketClaim['expiredAt']
+  }
+}
+
+const TicketForm: React.FC<TicketFormProps> = ({ ticket1, ticket2 }) => {
   const { toast } = useToast()
-  const [ticket1, setTicket1] = useState<HackathonTicket | null>(null)
-  const [ticket2, setTicket2] = useState<HackathonTicket | null>(null)
+  const router = useRouter()
 
   const code1Form = useForm<CodeSchema>({
     resolver: zodResolver(codeSchema),
@@ -40,14 +59,11 @@ const TicketForm: React.FC = () => {
     },
   })
 
-  const handleSubmit = async (
-    values: CodeSchema
-  ): Promise<HackathonTicket | null> => {
+  const onSubmit: SubmitHandler<CodeSchema> = async (values) => {
     try {
       const res = (await claimHackahonTicketWithRateLimit(
         values.code
       )) as Response<HackathonTicket>
-      console.log(res)
 
       if (!res.success) {
         toast({
@@ -55,10 +71,10 @@ const TicketForm: React.FC = () => {
           description: res.message,
           variant: 'destructive',
         })
-        return null
+        return
       }
 
-      return res.data
+      router.refresh()
     } catch (err) {
       toast({
         title: 'Error',
@@ -66,47 +82,36 @@ const TicketForm: React.FC = () => {
         variant: 'destructive',
       })
     }
-    return null
   }
 
-  const onSubmitTicket1: SubmitHandler<CodeSchema> = async (values) => {
-    const ticket = await handleSubmit(values)
-    if (!ticket) return
-    setTicket1(ticket)
-  }
-
-  const onSubmitTicket2: SubmitHandler<CodeSchema> = async (values) => {
-    const ticket = await handleSubmit(values)
-    if (!ticket) return
-    setTicket2(ticket)
+  const onCreateTeam = async (): Promise<void> => {
+    if (!ticket1 || !ticket2) return
+    try {
+      await createHackathonTeamTicket([ticket1.id, ticket2.id])
+    } catch (err) {
+      console.error(err instanceof Error ? err.message : err)
+    }
   }
 
   return (
     <div className='flex flex-col gap-24 sm:flex-row'>
       {ticket1 ? (
         <ClaimedTicket
-          expiredAt={new Date('17-02-2025')}
+          expiredAt={ticket1.expiredAt}
           ticketType={ticket1.ticketType}
         />
       ) : (
-        <TicketBox
-          form={code1Form}
-          name='Ticket 1'
-          onSubmit={onSubmitTicket1}
-        />
+        <TicketBox form={code1Form} name='Ticket 1' onSubmit={onSubmit} />
       )}
       {ticket2 ? (
         <ClaimedTicket
-          expiredAt={new Date('17-02-2025')}
+          expiredAt={ticket2.expiredAt}
           ticketType={ticket2.ticketType}
         />
       ) : (
-        <TicketBox
-          form={code2Form}
-          name='Ticket 2'
-          onSubmit={onSubmitTicket2}
-        />
+        <TicketBox form={code2Form} name='Ticket 2' onSubmit={onSubmit} />
       )}
+      {ticket1 && ticket2 ? <CreateTeamBox onSubmit={onCreateTeam} /> : null}
     </div>
   )
 }
