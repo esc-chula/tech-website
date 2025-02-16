94d652a21694e9576d197e7f3ed87f582091ae1b
ebonian <52095091+ebonian@users.noreply.github.com>
2025-02-16 22:13:18 +0700

    feat(hackathon): share story

--- src/app/(events)/hackathon/(registration)/registration/success/page.tsx
+++ src/app/(events)/hackathon/(registration)/registration/success/page.tsx
@@ -1,14 +1,29 @@
-import { notFound } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 import { findMyRegistration } from '~/server/actions/hackathon'
+
+import Building1Background from '../../../_components/common/bulding-1-background'
+import UserBox from '../../../_components/common/user-box'
+import ShareStory from '../../../_components/success/share-story'
 
 const Page: React.FC = async () => {
   const resMyRegistration = await findMyRegistration()
   if (!resMyRegistration.success || !resMyRegistration.data) {
-    return notFound()
+    return redirect(
+      `/hackathon/login?redirectUrl=/hackathon/registration/success`
+    )
   }
 
-  return <div>Your team is registered for the Intania Hackathon!</div>
+  return (
+    <>
+      <div className='flex min-h-dvh flex-col items-center gap-10 pb-24 pt-8'>
+        <UserBox />
+        <h1 className='text-4xl font-semibold md:text-5xl'>Complete</h1>
+        <ShareStory teamNo={23} />
+      </div>
+      <Building1Background hideHackathonTitle />
+    </>
+  )
 }
 
 export default Page
