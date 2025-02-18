2145a15d961d69adeca52cd5677e5885920511b6
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-02-19 00:43:07 +0700

    refactor(hackathon): constant names and simplify server actions

--- src/constants/hackathon.ts
+++ src/constants/hackathon.ts
@@ -5,6 +5,19 @@ export const HACKATHON_IG_URL =
   'https://intania.link/hack-ig?utm_source=web&utm_medium=corner+button'
 export const HACKATHON_OPENCHAT_URL =
   'https://intania.link/hack-openchat?utm_source=web&utm_medium=corner+button'
-export const HACKATHON_JACKPOT_RATE = 0.8
-export const HACKATHON_SYMBOLS = ['[7]', '<$>', '[B]', '(#)', '{*}', '[X]']
-export const HACKATHON_TICKET_CODES = ['ABCDEF', 'GHIJKL', 'MNOPQR', 'STUVWX']
+
+export const HACKATHON_GAME_JACKPOT_RATE = 0.2
+export const HACKATHON_GAME_JACKPOT_SYMBOLS = [
+  '[7]',
+  '<$>',
+  '[B]',
+  '(#)',
+  '{*}',
+  '[X]',
+]
+export const HACKATHON_GAME_JACKPOT_TICKET_CODES = [
+  'ABCDEF',
+  'GHIJKL',
+  'MNOPQR',
+  'STUVWX',
+]
