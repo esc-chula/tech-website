02ae0fb1f928e565c6c1766473093f7cf2279d61
ebonian <52095091+ebonian@users.noreply.github.com>
2025-02-18 17:27:09 +0700

    feat(hackathon): ticket code

--- src/app/(events)/hackathon/_components/ticket-releasing.tsx
+++ src/app/(events)/hackathon/_components/ticket-releasing.tsx
@@ -1,10 +1,22 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 export const TicketConsoleLog: React.FC = () => {
   console.log('DEV_GM0HP5G68B')
   return null
 }
 
 export const TransparentHeroText: React.FC = () => {
-  return <p className='absolute opacity-0'>hehe: </p>
+  const [random, setRandom] = useState<0 | 1>(0)
+
+  useEffect(() => {
+    setRandom(Math.floor(Math.random() * 2) as 0 | 1)
+  }, [])
+
+  return (
+    <p className='absolute opacity-0'>
+      {random === 0 ? 'hehe' : 'DEV_ITJI4UHDIO'}
+    </p>
+  )
 }
